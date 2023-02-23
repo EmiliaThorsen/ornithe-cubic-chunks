fba3f94605c146633d3ed3586319aa1de8d579dd
EmiliaThorsen <samuel.j.thors@gmail.com>
2023-02-23 14:29:50 +0100

    add the stuff i forgot to git add

---
+++ src/main/java/io/github/EmiliaThorsen/ornitheCubicChunks/mixin/TitleScreenMixin.java
@@ -0,0 +1,19 @@
+package io.github.EmiliaThorsen.ornitheCubicChunks.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.github.EmiliaThorsen.ornitheCubicChunks.ornitheCubicChunksMod;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+
+	@Inject(method = "init", at = @At("TAIL"))
+	public void ornitheCubicChunksMod$onInit(CallbackInfo ci) {
+		ornitheCubicChunksMod.LOGGER.info("ornithe cubic chunks mod is loaded!");
+	}
+}
---
+++ src/main/java/io/github/EmiliaThorsen/ornitheCubicChunks/ornitheCubicChunksMod.java
@@ -0,0 +1,13 @@
+package io.github.EmiliaThorsen.ornitheCubicChunks;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ornitheCubicChunksMod {
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod name as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("ornithe cubic chunks");
+
+}
